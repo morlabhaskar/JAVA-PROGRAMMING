2b3b1b25d06bde3730822e235dae1106602c00c1
morlabhaskar <morlabhaskar306@gmail.com>
2025-04-18 22:56:19 +0530

    Practice Problems

---
+++ practice/OPPs.java
@@ -0,0 +1,9 @@
+public class Main {
+    int x = 5;
+    public static void main(String[] args) {
+        Main obj1 = new Main();
+        Main obj2 = new Main();
+        System.out.println(obj1.x); // 5
+        System.out.println(obj2.x); // 5
+    }
+}
